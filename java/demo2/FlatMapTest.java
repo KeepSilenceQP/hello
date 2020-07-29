90f14953c889e89be4f6f35dee42edf8b1c6d057
KeepSilenceQP <qinpengzzj@gmail.com>
2020-07-29 21:26:05 +0800

    对 flatmap 加注释.
    Change-Id: I4450e5e9cbeef09772eee2e11b59a15d8d511fce

--- java/demo2/FlatMapTest.java
+++ java/demo2/FlatMapTest.java
@@ -5,11 +5,12 @@ import java.util.List;
 
 // map 起到的作用是转换, flatMap 则能够处理高维度转换, 将
 // 子管道中的数据平面展开到父管道中进行处理.
+// https://www.kancloud.cn/hanxt/javacrazy/1577825
 public class FlatMapTest {
     public static void main(String[] args) {
         List<String> ss = Arrays.asList("Hello", "World");
         ss.stream()
-            .map(s -> s.split(""))
+            .map(s -> s.split("")) // h e l l o
             .forEach(System.out::println);
         
         ss.stream()
